a9dce793a73c756f2ce6e33c402595e3baf279e0
NOORALDEIN FETAHI <117300676+n-fetahi@users.noreply.github.com>
2025-07-09 17:13:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/user.css',
-                'resources/js/user.js'
                 ],
             refresh: true,
         }),
